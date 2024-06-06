54b2dc3a2e3c219d769349fbb1ce77c55b4fffc3
sanipop <67932273+sanipop@users.noreply.github.com>
2024-06-06 13:34:43 +0100

    Create 5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' Description: input List and sum float
+    returns their sum as a float.
+    Arguments: input_list: List[float]
+'''
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''Outputs sum of all elements in input_list. '''
+    return sum(input_list)
