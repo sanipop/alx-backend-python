0edd799ebebdc3a83a74774bed81088154980644
sanipop <67932273+sanipop@users.noreply.github.com>
2024-06-06 13:03:20 +0100

    Create 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+''' Description:input float output string
+    representation of the float.
+    Parameters: n: float
+'''
+
+
+def to_str(n: float) -> str:
+    ''' outputs a string. '''
+    return str(n)
