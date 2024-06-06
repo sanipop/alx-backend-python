a2d13fa2853169295f6271be37a09c644f17490a
sanipop <67932273+sanipop@users.noreply.github.com>
2024-06-06 12:55:24 +0100

    Create 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+''' Description: concat str1 and 2
+    returns a concatenated string
+        parameters: str1: str
+               str2: str
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    ''' concat str1 and str2. '''
+    return str1 + str2
